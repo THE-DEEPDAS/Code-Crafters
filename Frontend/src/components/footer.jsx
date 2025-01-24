29a2caab5e53b6225a60432c7d0c0bf8faa9539c
Hitarth1810 <hmshahbrd931@gmail.com>
2025-01-25 03:45:03 +0530

    all ui changed

---
+++ Frontend/src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Footer = () => {
+    return (
+        <div className='flex flex-row bg-gray-900 p-9 justify-between'>
+            <p className='text-white'>© 2023 Your Company. All rights reserved.</p>
+            <Link
+              to="/contact"
+              className="text-white hover:text-blue-200 font-semibold"
+            >Contact Us</Link>
+        </div>
+    );
+};
+
+export default Footer;
