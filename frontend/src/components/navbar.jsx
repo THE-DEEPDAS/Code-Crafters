f99cf4d3daf6bbbc49339a43b2266e4b1ad721c8
Hitarth1810 <hmshahbrd931@gmail.com>
2025-01-25 01:22:48 +0530

    logo-added

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -2,60 +2,71 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className='bg-[#8Ed081] p-4 '>
-      <div className='grid grid-cols-2 container mx-auto'>
-        <ul className='flex flex-row gap-4'>
+    <nav className="bg-[#8Ed081] p-4 shadow-lg">
+      <div className="container mx-auto flex items-center justify-between">
+        {/* Logo */}
+        <div className="flex items-center">
+          <img
+            src=".\src\assets\ecobrew.png"
+            alt="Cup Karma Logo"
+            className="h-12 w-12 mr-4"
+          />
+          <h1 className="text-white font-bold text-xl">EcoBrew</h1>
+        </div>
+
+        {/* Navigation Links */}
+        <ul className="flex items-center gap-6">
           <li>
-            <Link to='/' className='text-white hover:text-blue-200 font-bold'>
+            <Link to="/" className="text-white hover:text-blue-200 font-semibold">
               Home
             </Link>
           </li>
           <li>
             <Link
-              to='/insights'
-              className='text-white hover:text-blue-200 font-bold'
+              to="/insights"
+              className="text-white hover:text-blue-200 font-semibold"
             >
               Insights
             </Link>
           </li>
           <li>
             <a
-              href='http://localhost:3000/encourage'
-              rel='noopener noreferrer'
-              className='text-white hover:text-blue-200 font-bold'
+              href="http://localhost:3000/encourage"
+              rel="noopener noreferrer"
+              className="text-white hover:text-blue-200 font-semibold"
             >
               Encourage
             </a>
           </li>
           <li>
             <a
-              href='https://cup-karma-insights.streamlit.app'
-              rel='noopener noreferrer'
-              className='text-white hover:text-blue-200 font-bold'
+              href="https://cup-karma-insights.streamlit.app"
+              rel="noopener noreferrer"
+              className="text-white hover:text-blue-200 font-semibold"
             >
               Recommendations
             </a>
           </li>
           <li>
             <Link
-              to='/contact'
-              className='text-white hover:text-blue-200 font-bold'
+              to="/contact"
+              className="text-white hover:text-blue-200 font-semibold"
             >
               Contact
             </Link>
           </li>
-        </ul>
-        <ul className='flex flex-row-reverse '>
           <li>
             <Link
-              to='/signin'
-              className='text-white hover:text-blue-200 pr-4 font-bold'
+              to="/signin"
+              className="bg-white text-[#8Ed081] hover:bg-gray-100 px-4 py-2 rounded-md font-semibold transition-colors duration-200"
             >
               Sign In
             </Link>
+          </li>
+          <li>
             <Link
-              to='/signup'
-              className='text-white hover:text-blue-200 pr-4  font-bold'
+              to="/signup"
+              className="bg-white text-[#8Ed081] hover:bg-gray-100 px-4 py-2 rounded-md font-semibold transition-colors duration-200"
             >
               Sign Up
             </Link>
@@ -64,7 +75,6 @@ const Navbar = () => {
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
-
